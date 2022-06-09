9b6d9d3e62e300095ae9d002673e605d9f3f26b2
YerayDP <73529878+YerayDP@users.noreply.github.com>
2022-06-09 22:42:14 +0200

    CONN12

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ return [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'database' => env($DATABASE_URL["path"], "/"),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
