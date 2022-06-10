274a00d9090329cf7597e0b698b4095df5e1eec6
YerayDP <yerayd99@gmail.com>
2022-06-10 03:02:22 +0200

    a

--- database/migrations/2022_05_31_000001_create_backgrounds_table.php
+++ database/migrations/2022_05_31_000001_create_backgrounds_table.php
@@ -13,6 +13,7 @@ class CreateBackgroundsTable extends Migration
      */
     public function up()
     {
+        Schema::defaultStringLength(191);
         Schema::create('backgrounds', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
--- database/migrations/2022_05_31_000002_create_weapons_table.php
+++ database/migrations/2022_05_31_000002_create_weapons_table.php
@@ -13,6 +13,7 @@ class CreateWeaponsTable extends Migration
      */
     public function up()
     {
+        Schema::defaultStringLength(191);
         Schema::create('weapons', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
